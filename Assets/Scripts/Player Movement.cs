f3a696be6477b71dee05c022370718632ab78dd4
Robomarti <robomarti@outlook.com>
2024-08-15 21:01:32 +0300

    added sea movement (not working yet)

--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -7,11 +7,26 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 movementDirection;
     [SerializeField] private InputActionReference movement;
 
+    [SerializeField] private Transform seaChunksParent;
+    private float lastFullXCoordinate;
+    private float lastFullZCoordinate;
+
+    private bool needToUpdateSeaChunkPosition = false;
+
     private void Update() {
         movementDirection = movement.action.ReadValue<Vector2>();
-    }
+        transform.position += movementSpeed * Time.deltaTime * new Vector3(movementDirection.x, 0, movementDirection.y);
+        if (Mathf.Floor(transform.position.x) != lastFullXCoordinate) {
+            lastFullXCoordinate = Mathf.Floor(transform.position.x);
+            needToUpdateSeaChunkPosition = true;
+        }
+        if (Mathf.Floor(transform.position.z) != lastFullZCoordinate) {
+            lastFullZCoordinate = Mathf.Floor(transform.position.z);
+            needToUpdateSeaChunkPosition = true;
+        }
 
-    private void FixedUpdate() {
-        transform.position += movementSpeed * Time.deltaTime * new Vector3(movementDirection.x, 0, movementDirection.y) ;
+        if (needToUpdateSeaChunkPosition) {
+            seaChunksParent.position = new Vector3(lastFullXCoordinate, seaChunksParent.position.y, lastFullZCoordinate);
+        }
     }
 }
