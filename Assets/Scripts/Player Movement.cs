8b5890233256d98f530ac5903ee6e6c30b86538b
Robomarti <robomarti@outlook.com>
2024-08-21 18:49:32 +0300

    fine tuned sea chunk movement

--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -13,20 +13,27 @@ public class PlayerMovement : MonoBehaviour
 
     private bool needToUpdateSeaChunkPosition = false;
     [SerializeField] private WaveManager waveManager;
+    private int largestLevelOfDetail = -1;
+
+    private void Start() {
+        GameObject seaManager = GameObject.Find("Sea Manager");
+        largestLevelOfDetail = seaManager.GetComponent<CreateSeaChunks>().detailLevels[^1].levelOfDetail;
+    }
 
     private void Update() {
+        // Player movement
         movementDirection = movement.action.ReadValue<Vector2>();
         transform.position += movementSpeed * Time.deltaTime * new Vector3(movementDirection.x, 0, movementDirection.y);
 
-        // Move sea chunks
-        // We check if the difference in position is larger than 6 because 5 is the
-        // largest level of detail, and each LOD 5 chunk's cube is 6 times larger than
-        // a LOD 0 chunk's cube
-        if (Mathf.Abs(Mathf.Floor(transform.position.x) - lastFullXCoordinate) >= 6) {
+        // Sea chunks movement.
+        // We check if the difference in position is larger than largestLevelOfDetail+1 
+        // because cubes of a LOD largestLevelOfDetail chunk are largestLevelOfDetail+1 times larger than
+        // cubes of a LOD 0 chunk
+        if (Mathf.Abs(Mathf.Floor(transform.position.x) - lastFullXCoordinate) >= largestLevelOfDetail+1) {
             lastFullXCoordinate = Mathf.Floor(transform.position.x);
             needToUpdateSeaChunkPosition = true;
         }
-        if (Mathf.Abs(Mathf.Floor(transform.position.z) - lastFullZCoordinate) >= 6) {
+        if (Mathf.Abs(Mathf.Floor(transform.position.z) - lastFullZCoordinate) >= largestLevelOfDetail+1) {
             lastFullZCoordinate = Mathf.Floor(transform.position.z);
             needToUpdateSeaChunkPosition = true;
         }
