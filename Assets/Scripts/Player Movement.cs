a2415c4bd2caa358aa60aad2d04420bf297f4c73
Robomarti <robomarti@outlook.com>
2024-08-21 18:34:35 +0300

    somewhat fixed sea chunk jittering

--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -17,11 +17,16 @@ public class PlayerMovement : MonoBehaviour
     private void Update() {
         movementDirection = movement.action.ReadValue<Vector2>();
         transform.position += movementSpeed * Time.deltaTime * new Vector3(movementDirection.x, 0, movementDirection.y);
-        if (Mathf.Floor(transform.position.x) != lastFullXCoordinate) {
+
+        // Move sea chunks
+        // We check if the difference in position is larger than 6 because 5 is the
+        // largest level of detail, and each LOD 5 chunk's cube is 6 times larger than
+        // a LOD 0 chunk's cube
+        if (Mathf.Abs(Mathf.Floor(transform.position.x) - lastFullXCoordinate) >= 6) {
             lastFullXCoordinate = Mathf.Floor(transform.position.x);
             needToUpdateSeaChunkPosition = true;
         }
-        if (Mathf.Floor(transform.position.z) != lastFullZCoordinate) {
+        if (Mathf.Abs(Mathf.Floor(transform.position.z) - lastFullZCoordinate) >= 6) {
             lastFullZCoordinate = Mathf.Floor(transform.position.z);
             needToUpdateSeaChunkPosition = true;
         }
